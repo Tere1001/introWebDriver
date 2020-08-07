2773ac4ce333026954b5187b2ac458df67b2073d
Tere1001 <tzmedel@gmail.com>
2020-08-07 01:12:49 -0500

    Se agregan cambios

--- src/test/java/bIntro/Constants.java
+++ src/test/java/bIntro/Constants.java
@@ -2,5 +2,5 @@ package bIntro;
 
 public class Constants {
     public static final String USUARIO = "tzmedel@gmail.com";
-    public static final String PASSWORD = "elodia_123";
+    public static final String PASSWORD = "Prueba123";
 }
